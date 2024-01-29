efa5a313f31364c8b25333bb7df061170e6aeda3
Nathan Wing <wing_nathan@hotmail.com>
2024-01-29 16:29:47 +0000

    Table Migrations created for Time Column

--- Acebook/Models/Post.cs
+++ Acebook/Models/Post.cs
@@ -8,4 +8,18 @@ public class Post
   public string? Content {get; set;}
   public int UserId {get; set;}
   public User? User {get; set;}
+
+  public DateTime Time {get; set;}
+
+
+  public Post(string Content, int user_id, User user, DateTime time) {
+    this.Content = Content;
+    this.UserId = user_id;
+    this.User = user;
+    this.Time = time;
+  }
+
+  public Post() {
+    
+  }
 }
