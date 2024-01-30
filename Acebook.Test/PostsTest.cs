c46b9647242787069c0738cbc43251791660bacf
Nathan Wing <wing_nathan@hotmail.com>
2024-01-30 11:16:09 +0000

    Tests for Posts written and passing.
    Co-authored-by: Harry Dickson <HarryFDickson@users.noreply.github.com>

---
+++ Acebook.Test/PostsTest.cs
@@ -0,0 +1,65 @@
+using OpenQA.Selenium.Internal;
+
+namespace Acebook.Test;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics.CodeAnalysis;
+
+public class PostsTests
+{
+    ChromeDriver driver;
+
+    [SetUp]
+
+    public void Setup()
+    {
+        driver = new ChromeDriver();
+        SignInUser();
+    }
+
+    [TearDown]
+    public void TearDown() 
+    {
+    driver.Quit();
+    }
+
+    [Test]
+
+    public void IndexPageHasPostInTitle()
+    {
+        
+        string currentUrl = driver.Url;
+        Assert.AreEqual("http://127.0.0.1:5287/posts", currentUrl);
+        IWebElement title = driver.FindElement(By.Id("title"));
+        Assert.AreEqual("Posts", title.GetAttribute("innerHTML"));
+    }
+
+    public void WhenUserCreatesNewPostItAppearsOnPage()
+    {
+        string currentUrl = driver.Url;
+        IWebElement emailField = driver.FindElement(By.Name("content"));
+        emailField.SendKeys("Harry is sitting on his throne.");
+        IWebElement submitButton = driver.FindElement(By.Id("submit_button"));
+        submitButton.Click();
+        Assert.AreEqual("http://127.0.0.1:5287/posts", currentUrl);
+        IWebElement content = driver.FindElement(By.TagName("p"));
+        Assert.AreEqual("Harry is sitting on his throne.", content.GetAttribute("innerHTML"));
+
+
+        
+    }
+
+    private void SignInUser()
+        {
+            driver.Navigate().GoToUrl("http://127.0.0.1:5287/signin");
+            IWebElement emailField = driver.FindElement(By.Id("email"));
+            emailField.SendKeys("francine@email.com");
+
+            IWebElement passwordField = driver.FindElement(By.Id("password"));
+            passwordField.SendKeys("12345678");
+
+            IWebElement submitButton = driver.FindElement(By.Id("submit"));
+            submitButton.Click();
+        }
+}
