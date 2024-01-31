5acf69bf841453304f37ed45c16d00140679f85f
Harry <harry_dickson@icloud.com>
2024-01-31 12:35:53 +0000

    Long day

--- Acebook.Test/PostsTest.cs
+++ Acebook.Test/PostsTest.cs
@@ -6,6 +6,8 @@ using OpenQA.Selenium;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics.CodeAnalysis;
 using acebook.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 public class PostsTests
 {
@@ -15,7 +17,7 @@ public class PostsTests
 
     public void Setup()
     {
-        CleanupDatabase();
+        
         driver = new ChromeDriver();
         SignInUser();
     }
@@ -23,7 +25,8 @@ public class PostsTests
     [TearDown]
     public void TearDown() 
     {
-    driver.Quit();
+        CleanupDatabase();
+        driver.Quit();
     }
 
     [Test]
@@ -61,18 +64,27 @@ public class PostsTests
 
     [Test]
     public void WhenClickingOnPostItTakesYouToPostInDetails(){
-        // Un-comment when hyperlinks are fixed
-        // IList <IWebElement> hyperlinkButton = driver.FindElements(By.TagName("href"));
-        // foreach(IWebElement e in hyperlinkButton) {
-        // if (e.Text == "Comment")
-        // {
-        //     e.Click();
-        //     break;
-        // }
-        // }
-        driver.Navigate().GoToUrl("http://127.0.0.1:5287/posts/1");
+        IWebElement passwordField = driver.FindElement(By.Name("content"));
+        passwordField.SendKeys("Post");
+
+        IWebElement submitButton = driver.FindElement(By.Id("submit_button"));
+        submitButton.Click();
+        IWebElement commentButton = driver.FindElement(By.CssSelector("a.btn.btn-primary"));
+        commentButton.Click();
+
         string currentUrl = driver.Url;
-        Assert.That("http://127.0.0.1:5287/posts/1", Is.EqualTo(currentUrl));
+        int idNum = 0;
+        using (var dbContext = new AcebookDbContext())
+        {
+            foreach (Post post in dbContext.Posts)
+            {
+                idNum = post.Id;
+            }
+        }
+
+        Assert.That($"http://127.0.0.1:5287/posts/{idNum}", Is.EqualTo(currentUrl));
+
+
         IList <IWebElement> elements = driver.FindElements(By.TagName("p"));
         bool isFound = false;
         foreach(IWebElement e in elements) {
@@ -88,7 +100,12 @@ public class PostsTests
     //Do not turn this into a test
     private void SignInUser()
         {
-            driver.Navigate().GoToUrl("http://127.0.0.1:5287/signin");
+            driver.Navigate().GoToUrl("http://127.0.0.1:5287");
+            IWebElement signUpButton = driver.FindElement(By.Id("signup"));
+            signUpButton.Click();
+            IWebElement nameField = driver.FindElement(By.Id("name"));
+            nameField.SendKeys("francine");
+
             IWebElement emailField = driver.FindElement(By.Id("email"));
             emailField.SendKeys("francine@email.com");
 
@@ -97,6 +114,17 @@ public class PostsTests
 
             IWebElement submitButton = driver.FindElement(By.Id("submit"));
             submitButton.Click();
+
+
+            driver.Navigate().GoToUrl("http://127.0.0.1:5287/signin");
+            emailField = driver.FindElement(By.Id("email"));
+            emailField.SendKeys("francine@email.com");
+
+            passwordField = driver.FindElement(By.Id("password"));
+            passwordField.SendKeys("12345678");
+
+            submitButton = driver.FindElement(By.Id("submit"));
+            submitButton.Click();
         }
     private void CleanupDatabase()
     {
@@ -104,6 +132,7 @@ public class PostsTests
         {
             dbContext.Posts.RemoveRange(dbContext.Posts);
             dbContext.Users.RemoveRange(dbContext.Users);
+
             dbContext.SaveChanges();
         }
     }
