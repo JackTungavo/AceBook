2416279e2ed3a3c7a8e561a2e324ae70703a687c
jacktungavo <jack10tungavo@gmail.com>
2024-02-02 11:07:35 +0000

    resolved problems found in workspace

--- Acebook/Controllers/PostsController.cs
+++ Acebook/Controllers/PostsController.cs
@@ -2,6 +2,9 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using acebook.Models;
 using acebook.ActionFilters;
+using Microsoft.VisualBasic;
+using System.Reflection.Metadata;
+using Microsoft.EntityFrameworkCore;
 
 namespace acebook.Controllers;
 
@@ -10,7 +13,6 @@ public class PostsController : Controller
 {
   private readonly ILogger<PostsController> _logger;
   AcebookDbContext dbContext = new AcebookDbContext(); 
-
   public PostsController(ILogger<PostsController> logger)
   {
     _logger = logger;
@@ -20,14 +22,17 @@ public class PostsController : Controller
   [HttpGet]
   public IActionResult Index() 
   {
-    int currentUserId = HttpContext.Session.GetInt32("user_id").Value;
-    List<Post> posts = dbContext.Posts.ToList();
-    ViewBag.Posts = posts;
-    if (currentUserId != null)
+    var currentUserId = HttpContext.Session.GetInt32("user_id");
+    if (currentUserId != null) 
     {
-        ViewBag.Id = currentUserId;
+      ViewBag.Id = (int)currentUserId.Value;
     }
     
+    if (dbContext.Posts != null) 
+    {
+      List<Post> posts = dbContext.Posts.ToList();
+      ViewBag.Posts = posts;
+    }
     return View();
   }
 
@@ -35,16 +40,18 @@ public class PostsController : Controller
   [HttpPost]
   public RedirectResult Create(Post post) 
   {
-    //AcebookDbContext dbContext = new AcebookDbContext();
-    int currentUserId = HttpContext.Session.GetInt32("user_id").Value;
-    post.UserId = currentUserId;
-    post.Time = DateTime.UtcNow;
-    post.UsersLiked = string.Empty;
-    dbContext.Posts.Add(post);
-    dbContext.SaveChanges();
-  //   post.PostParentId = post.Id;
-  //   dbContext.SaveChanges();
+    var currentUserId = HttpContext.Session.GetInt32("user_id");
+    if (currentUserId != null) {
+      post.UserId = (int)currentUserId.Value;
+      post.Time = DateTime.UtcNow;
+      post.UsersLiked = string.Empty;
+    }
 
+    if (dbContext.Posts != null) 
+    {
+      dbContext.Posts.Add(post);
+      dbContext.SaveChanges();
+    }
     return new RedirectResult("/posts");
   }
 
@@ -52,47 +59,45 @@ public class PostsController : Controller
   [HttpGet]
   public IActionResult Show(int id)
   {
-  //AcebookDbContext dbContext = new AcebookDbContext();
-  Post post = dbContext.Posts.FirstOrDefault(p => p.Id == id);
-  post.Likes = GetLikesFromPost(post);
+    if (dbContext.Posts == null) {return RedirectToAction("Index");}
+    Post post = dbContext.Posts.FirstOrDefault(p => p.Id == id) ?? new Post();
+    if (post == null)
+    {
+        // Handle the case where the post with the specified ID is not found
+        return RedirectToAction("Index");
+    }
+    post.Likes = GetLikesFromPost(post);
+    List<Post> comments = dbContext.Posts.Where(c => c.PostParentId == id).ToList();
+    foreach (Post comment in comments)
+    {
+      comment.Likes = GetLikesFromPost(comment);
+    }
+    dbContext.SaveChanges();
+    // Pass the post and comments to the view
 
-  if (post == null)
-  {
-      // Handle the case where the post with the specified ID is not found
-      return RedirectToAction("Index");
+    List<Post> sortedByTimeCommments = comments.OrderByDescending(comment => comment.Time).ToList();
+    ViewBag.Post = post;
+    ViewBag.Comments = sortedByTimeCommments;//comments;
+
+    return View(post);
   }
-
-  List<Post> comments = dbContext.Posts.Where(c => c.PostParentId == id).ToList();
-  foreach (Post comment in comments)
-  {
-    comment.Likes = GetLikesFromPost(comment);
-  }
-  dbContext.SaveChanges();
-  // Pass the post and comments to the view
-
-  List<Post> sortedByTimeCommments = comments.OrderByDescending(comment => comment.Time).ToList();
-  ViewBag.Post = post;
-  ViewBag.Comments = sortedByTimeCommments;//comments;
-
-  return View(post);
-  
-}
 
   [Route("/posts/comment")]
   [HttpPost]
   public RedirectResult AddComment(int postParentId, Post comment)
   {
-    //AcebookDbContext dbContext = new AcebookDbContext();
-    int currentUserId = HttpContext.Session.GetInt32("user_id").Value;
+    var currentUserId = HttpContext.Session.GetInt32("user_id");
 
     // Comment
-    comment.UserId = currentUserId;
-    comment.Time = DateTime.UtcNow;
-    comment.PostParentId = postParentId;
-    comment.UsersLiked = string.Empty;
-    dbContext.Posts.Add(comment);
-    dbContext.SaveChanges();
-
+    if (dbContext.Posts != null && currentUserId != null) 
+    {
+      comment.UserId = (int)currentUserId.Value;
+      comment.Time = DateTime.UtcNow;
+      comment.PostParentId = postParentId;
+      comment.UsersLiked = string.Empty;
+      dbContext.Posts.Add(comment);
+      dbContext.SaveChanges();
+    }
     return new RedirectResult($"/posts/{postParentId}");
   }
 
@@ -101,31 +106,43 @@ public class PostsController : Controller
 
   public RedirectResult LikePost(int id)
   {
-    //AcebookDbContext dbContext = new AcebookDbContext();
-    int currentUserId = HttpContext.Session.GetInt32("user_id").Value;
-    Post post = dbContext.Posts.Find(id);
-    post.LikePost(currentUserId);
-    post.Likes = GetLikesFromPost(post);
-    dbContext.Posts.Update(post);
-    dbContext.SaveChanges();
-    if (post.PostParentId == 0) {return new RedirectResult($"/posts/{id}");}
-    else {return new RedirectResult($"/posts/{post.PostParentId}");}
+    var currentUserId = HttpContext.Session.GetInt32("user_id");
+    if (dbContext.Posts != null && currentUserId != null) 
+    {
+      Post post = dbContext.Posts.Find(id) ?? new Post();
+      post.LikePost((int)currentUserId.Value);
+      post.Likes = GetLikesFromPost(post);
+      dbContext.Posts.Update(post);
+      dbContext.SaveChanges();
+      if (post.PostParentId == 0) 
+      {
+        return new RedirectResult($"/posts/{id}");
+      }
+      else 
+      {
+        return new RedirectResult($"/posts/{post.PostParentId}");
+      }
+    }
+    else
+    {
+      return new RedirectResult($"/posts/{id}");
+    }
   }
 
   
   public int GetLikesFromPost(Post post)
   {
-    //AcebookDbContext dbContext = new AcebookDbContext();
     int finalLikes = 0;
-
-    foreach (User user in dbContext.Users)
-    {
-      if(post.UsersLiked.Contains("'"+user.Id.ToString()+"'"))
-        {
-          finalLikes ++;
-        }
-    } 
-      return finalLikes;
+    if (dbContext.Users != null && post.UsersLiked != null) {
+      foreach (User user in dbContext.Users)
+      {
+        if(post.UsersLiked.Contains("'"+user.Id.ToString()+"'"))
+          {
+            finalLikes ++;
+          }
+      } 
+    }    
+    return finalLikes;
   }
 
   [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
